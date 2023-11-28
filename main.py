2d7a885a8d3d1847961439e2df4ac6c7540d7b29
Aleksei Kaliagin <>
2023-11-29 00:57:07 +0300

    Fix main

--- main.py
+++ main.py
@@ -147,10 +147,7 @@ def predict_items(file: UploadFile = File(...)) -> FileResponse:
     df[["year", "km_driven", "mileage", "engine", "max_power"]] = sc.transform(
         df[["year", "km_driven", "mileage", "engine", "max_power"]])
 
-    car = SchemaCSV(**df)
-    data = car.model_dump()
-    df = pd.DataFrame([data])
-
+    df = df[lr_r.feature_names_in_]
     predictions = lr_r.predict(df)
 
     df_with_predictions = pd.concat([df, predictions], axis=1)
