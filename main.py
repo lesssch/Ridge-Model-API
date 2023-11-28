a07f3a715fed33a566807a8bf7c111217ce8b18b
Aleksei Kaliagin <>
2023-11-28 14:59:34 +0300

    Fix main

--- main.py
+++ main.py
@@ -31,6 +31,31 @@ class Item(BaseModel):
 class Items(BaseModel):
     objects: List[Item]
 
+class Schema(BaseModel):
+    year: int
+    km_driven: int
+    mileage: float
+    engine: float
+    max_power: float
+    fuel_Diesel: int = 0
+    fuel_LPG: int = 0
+    fuel_Petrol: int = 0
+    seller_type_Individual: int = 0
+    seller_type_Trustmark Dealer: int = 0
+    transmission_Manual: int = 0
+    owner_Fourth & Above Owner: int = 0
+    owner_Second Owner: int = 0
+    owner_Test Drive Car: int = 0
+    owner_Third Owner: int = 0
+    seats_4: int = 0
+    seats_5: int = 0
+    seats_6: int = 0
+    seats_7: int = 0
+    seats_8: int = 0
+    seats_9: int = 0
+    seats_10: int = 0
+    seats_14: int = 0
+
 
 
 @app.post("/predict_item")
@@ -39,8 +64,6 @@ def predict_item(item: Item) -> float:
     car_instance = Item(**car)
     data = car_instance.model_dump()
     df = pd.DataFrame([data])
-    df = df.drop_duplicates(keep="first", subset=df.columns.difference(["selling_price"]))
-    df.reset_index(drop=True)
 
     df["mileage"] = df["mileage"].str.extract('(\d*\.?\d*)').astype(float)
     df["engine"] = df["engine"].str.extract('(\d+)').astype(float)
@@ -56,12 +79,16 @@ def predict_item(item: Item) -> float:
     df["seats"] = df["seats"].astype(int)
 
     df = df.drop(["selling_price", "name"], axis=1)
-    df = pd.get_dummies(data=df, columns=["fuel", "seller_type", "transmission", "owner", "seats"],
-                                 drop_first=True, prefix_sep="_", dtype=int)
+    df = pd.get_dummies(data=df, columns=["fuel", "seller_type", "transmission", "owner", "seats"], prefix_sep="_", dtype=int)
 
     sc = StandardScaler()
     df[["year", "km_driven", "mileage", "engine", "max_power"]] = sc.fit_transform(
         df[["year", "km_driven", "mileage", "engine", "max_power"]])
+
+    df.to_dict()
+    car = Schema(**df)
+    data = car.model_dump()
+    df = pd.DataFrame([data])
 
     predict = lr_r.predict(df)
 
